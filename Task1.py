59e3531155faf161c9649d846ba0f709e77e40bf
liyi201809 <liyi201809@gmail.com>
2019-06-18 23:35:23 +0200

    the preprocess of data

--- Task1.py
+++ Task1.py
@@ -1,8 +1,8 @@
 # -StartPython.py *- coding: utf-8 -*-
 from loaddata import load_data 
+from loaddata import load_ip_sequence
 import json
-
+#%%
 dataset = load_data('C:/Users/YI/Desktop/TUD/Cyber data analytics/LAB3/Sampling/capture20110811.pcap.netflow.labeled')
-
-#with open('botnet-capture-20110819-bot.json', 'r',encoding="utf8", errors='ignore') as csvDataFile:
-#    csvReader = json.load(csvDataFile)
+#%%
+ip_data = load_ip_sequence(dataset,'147.32.84.229')
